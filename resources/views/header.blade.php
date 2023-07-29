0885c6fb1963c1a569faf8aa1aea9ea58a50b7fa
breadthe <omigoshdev@protonmail.com>
2023-07-29 00:27:41 -0500

    named routes to indicated selected page in header

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,8 +1,12 @@
+@php
+use \Illuminate\Support\Facades\Route;
+@endphp
+
 @section('header')
     <ul class="flex gap-2">
-        <li><a href="/">Home</a></li>
-        <li><a href="/history">History</a></li>
-        <li><a href="/settings">Settings</a></li>
+        <li><a href="/" class="{{ Route::is('index') ? 'text-lime-600 font-bold' : '' }}">Home</a></li>
+        <li><a href="/history" class="{{ Route::is('history') ? 'text-lime-600 font-bold' : '' }}">History</a></li>
+        <li><a href="/settings" class="{{ Route::is('settings') ? 'text-lime-600 font-bold' : '' }}">Settings</a></li>
     </ul>
     <livewire:api-key />
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', fn () => view('pages.index'))->name('index');
+Route::get('/history', fn () => view('pages.history.index'))->name('history');
+Route::get('/settings', fn () => view('pages.settings.index'))->name('settings');

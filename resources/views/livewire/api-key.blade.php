7b5f1438b4b27699ee6a5ee497e23f07ef7b4ee0
breadthe <omigoshdev@protonmail.com>
2023-07-28 23:09:29 -0500

    api key registration component

---
+++ resources/views/livewire/api-key.blade.php
@@ -0,0 +1,72 @@
+<?php
+
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Log;
+use function Livewire\Volt\{state, mount, rules};
+
+state(['settings' => null]);
+state(['apiKey' => null]);
+state(['newApiKey' => null]);
+state(['isShowingForm' => false]);
+state(['isShowingApiKey' => false]);
+
+rules(['newApiKey' => 'required|string']);
+
+mount(function () {
+    $this->settings = Storage::json('settings.json');
+    $this->apiKey = $this->settings['api_key'];
+});
+
+$save = function () {
+    $this->validate();
+    $this->isShowingApiKey = false;
+
+    try {
+        $this->apiKey = $this->newApiKey;
+        $this->settings['api_key'] = $this->apiKey;
+
+        Storage::put('settings.json', json_encode($this->settings));
+
+        $this->newApiKey = null;
+        $this->isShowingForm = false;
+    } catch (\Exception $e) {
+        Log::error($e->getMessage());
+    }
+};
+?>
+
+<div class="w-full flex flex-col items-center gap-2 py-2 bg-lime-950">
+    @if($apiKey)
+        <div class="flex gap-4">
+            <span>API key ✅</span>
+            <button type="button" wire:click="$toggle('isShowingApiKey')" class="btn-link" title="Show API key">
+                @if($isShowingApiKey)
+                    🙈 hide
+                @else
+                    👀 show
+                @endif
+            </button>
+            <button type="button" wire:click="$toggle('isShowingForm')" class="btn-link" title="Change API key">♻️ change</button>
+        </div>
+
+        @if($isShowingApiKey)
+            <div class="flex gap-4">
+                <span>{{ $apiKey }}</span>
+            </div>
+        @endif
+    @endif
+
+    @if($isShowingForm)
+        <form wire:submit.prevent="save" class="w-full flex flex-col items-center">
+            <div class="flex sm:flex-row flex-col gap-2">
+                <input type="text" id="api_key" wire:model="newApiKey" placeholder="API key">
+                <button type="submit">save</button>
+                <button type="button" wire:click="$toggle('isShowingForm')" class="btn-link">cancel</button>
+            </div>
+
+            <div class="w-full text-center bg-lime-900">
+                @error('newApiKey') <span class="text-lime-500">{{ $message }}</span> @enderror
+            </div>
+        </form>
+    @endif
+</div>

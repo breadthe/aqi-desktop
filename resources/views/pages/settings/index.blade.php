9d044acab0df123d540aa16d986027205c8bb4ea
breadthe <omigoshdev@protonmail.com>
2023-07-31 23:11:01 -0500

    settings styling

--- resources/views/pages/settings/index.blade.php
+++ resources/views/pages/settings/index.blade.php
@@ -5,12 +5,12 @@
 @endsection
 
 @section('content')
-    <div class="w-full flex flex-col mx-auto py-2 px-8">
-        <div class="w-full flex flex-col sm:flex-row gap-4 items-start">
-            <div class="sm:w-1/4 sm:text-right">
+    <div class="w-full flex flex-col mx-auto py-2">
+        <div class="w-full flex flex-col sm:flex-row gap-4 items-start px-8">
+            <div class="flex flex-col sm:w-1/4 sm:text-right">
                 <strong>API key</strong>
             </div>
-            <div class="sm:w-3/4">
+            <div class="w-full sm:w-3/4">
                 <livewire:api-key />
             </div>
         </div>

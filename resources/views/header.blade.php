e3a8900cd9204b198f2e523ce7e229c7ef0435ec
breadthe <omigoshdev@protonmail.com>
2023-07-31 23:30:45 -0500

    rename index route to location

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -5,7 +5,7 @@ use \Illuminate\Support\Facades\Route;
 @section('header')
     <ul class="flex items-center justify-center gap-8 mx-auto p-2">
         <li>
-            <a wire:navigate href="/" class="flex flex-col items-center{{ Route::is('index') ? ' text-lime-600' : ' opacity-70' }}" title="Location">
+            <a wire:navigate href="/" class="flex flex-col items-center{{ Route::is('location') ? ' text-lime-600' : ' opacity-70' }}" title="Location">
                 <svg viewBox="0 0 20 20" fill="currentColor" class="w-6 h-6">
                     <path fill-rule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clip-rule="evenodd"/>
                 </svg>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', fn () => view('pages.index'))->name('index');
+Route::get('/', fn () => view('pages.index'))->name('location');
 Route::get('/history', fn () => view('pages.history.index'))->name('history');
 Route::get('/settings', fn () => view('pages.settings.index'))->name('settings');

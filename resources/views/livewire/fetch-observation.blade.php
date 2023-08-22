1eab4110c24030e5b14b31a745a13e06b890f404
breadthe <omigoshdev@protonmail.com>
2023-08-21 22:43:30 -0500

    zip history

--- resources/views/livewire/fetch-observation.blade.php
+++ resources/views/livewire/fetch-observation.blade.php
@@ -2,9 +2,12 @@
 
 use App\Models\Observation;
 use Carbon\Carbon;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 use Native\Laravel\Facades\Settings;
-use function Livewire\Volt\{state, mount, boot, rules};
+use Illuminate\Support\Facades\DB;
+use function Livewire\Volt\{state, mount, boot, rules, on};
 
 // settings
 state(['apiKey' => null]);
@@ -13,6 +16,7 @@ state(['lastUpdatedAt' => null]);
 
 state(['newObservation' => null]);
 state(['fetchError' => false]);
+state(['zipHistory' => collect()]);
 
 rules(['zip' => 'required|numeric|digits:5']);
 
@@ -20,6 +24,8 @@ mount(function () {
     if ($this->zip) {
         $this->fetch();
     }
+
+    $this->zipHistory = $this->getZipHistory();
 });
 
 boot(function () {
@@ -27,6 +33,9 @@ boot(function () {
     $this->zip = Settings::get('last_zip');
     $this->lastUpdatedAt = Settings::get('last_updated_at');
 });
+
+// @todo why is zipChanged fired twice???
+on(['zipChanged' => fn ($zip) => $this->changeZip($zip)]);
 
 $fetch = function () {
     $this->fetchError = false;
@@ -40,25 +49,30 @@ $fetch = function () {
     Settings::set('last_zip', $this->zip);
 
     // @todo try catch
-    $response = Http::get($url);
+    try {
+        $response = Http::get($url);
 
-    // @todo catch invalid api key
+        // @todo catch invalid api key
 
-    $this->lastUpdatedAt = now();
-    $this->newObservation = $response->json();
+        $this->lastUpdatedAt = now();
+        $this->newObservation = $response->json();
 
-    if (is_array($this->newObservation) && empty($this->newObservation)) {
-        $this->fetchError = true;
+        if (is_array($this->newObservation) && empty($this->newObservation)) {
+            $this->fetchError = true;
 
-        return;
+            return;
+        }
+
+        Settings::set('last_updated_at', $this->lastUpdatedAt);
+        Settings::set('last_observation', $this->newObservation);
+        $this->zipHistory = $this->getZipHistory();
+
+        $this->saveObservation();
+
+        $this->dispatch('observation-updated');
+    } catch (\Exception $e) {
+        Log::error($e->getMessage());
     }
-
-    Settings::set('last_updated_at', $this->lastUpdatedAt);
-    Settings::set('last_observation', $this->newObservation);
-
-    $this->saveObservation();
-
-    $this->dispatch('observation-updated');
 };
 
 $saveObservation = function () {
@@ -88,17 +102,37 @@ $saveObservationParameter = function (array $parameter) {
         ]
     );
 };
+
+$getZipHistory = function (): Collection {
+    try {
+        return DB::table('observations')
+            ->selectRaw('DISTINCT zip_code, reporting_area')
+            ->orderBy('zip_code')
+            ->get();
+    } catch (\Exception $e) {
+        Log::error($e->getMessage());
+    }
+
+    return collect();
+};
+
+$changeZip = function (string $zip): void {
+    $this->zip = $zip;
+    Settings::set('last_zip', $this->zip);
+};
 ?>
 
 <div class="flex flex-col gap-4 py-2 px-8" @class(['h-full' => isset($apiKey)])>
     @empty($apiKey)
         <div class="w-full text-center bg-lime-900 rounded my-auto p-4">
-            <span class="text-lime-500">No AirNow API key, register it in <a href="{{ route('settings') }}" class="underline">Settings</a></span>
+            <span class="text-lime-500">No AirNow API key, register it in <a href="{{ route('settings') }}"
+                                                                             class="underline">Settings</a></span>
         </div>
     @else
         <form wire:submit.prevent="fetch" class="w-full flex flex-col items-center gap-2">
-            <div class="w-full flex items-center justify-center gap-2">
-                <input type="text" id="zip" wire:model="zip" maxlength="5" placeholder="Zip code" class="">
+            <div class="w-full flex items-center justify-center gap-2 relative" x-data="{showHistory: false}">
+                <input type="text" id="zip" wire:model="zip" maxlength="5" placeholder="Zip code"
+                       @focus="showHistory = true">
                 <button type="submit">
                     <svg viewBox="0 0 20 20" fill="currentColor" class="w-6 h-6">
                         <path fill-rule="evenodd"
@@ -106,6 +140,28 @@ $saveObservationParameter = function (array $parameter) {
                               clip-rule="evenodd"/>
                     </svg>
                 </button>
+
+                @if($zipHistory->count())
+                    <div x-show="showHistory" x-cloak
+                         class="absolute top-12 z-10 w-64 h-64 overflow-hidden rounded bg-lime-700 border border-lime-600 text-white">
+                        <div class="flex justify-between border-b border-lime-600">
+                            <span>&nbsp;</span>
+                            <span class="font-bold text-center">History</span>
+                            <button type="button" class="btn-close" @click="showHistory = false">&times;</button>
+                        </div>
+                        <ul class="h-full overflow-y-auto pb-8">
+                            @foreach($zipHistory as $zipHistoryEntry)
+                                <li class="text-left">
+                                    <button type="button"
+                                            class="btn-zip-history"
+                                            @click.once="$dispatch('zipChanged', {zip: '{{ $zipHistoryEntry->zip_code }}'}); showHistory = false">
+                                        {{ $zipHistoryEntry->zip_code }} &mdash; {{ $zipHistoryEntry->reporting_area }}
+                                    </button>
+                                </li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
             </div>
 
             @if($lastUpdatedAt)
